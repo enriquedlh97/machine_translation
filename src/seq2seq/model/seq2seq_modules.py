37e2ffb9049249b4e311fc9a54f30211c59d95cf
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-15 15:49:55 -0500

    Starts implementation of Encoder and Decoder modules

--- src/seq2seq/model/seq2seq_modules.py
+++ src/seq2seq/model/seq2seq_modules.py
@@ -4,4 +4,14 @@ Created: July 15, 2021
 Updated: July 15, 2021
 
 Description:
+    Implements the Encoder and Decoder modules used for the seq2seq model
 """
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    pass
+
+class Decoder(nn.Module):
+    pass
