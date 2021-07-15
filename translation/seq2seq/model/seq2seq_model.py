703891eb7fd1ca879e523345935bd1c7772dfcd7
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-15 16:33:36 -0500

    Finishes implementation of seq2seq model

--- translation/seq2seq/model/seq2seq_model.py
+++ translation/seq2seq/model/seq2seq_model.py
@@ -6,9 +6,44 @@ Updated: July 15, 2021
 Description:
     Implements the seq2seq model
 """
+import torch
 import torch.nn as nn
-from trans
+import random
 
 
 class Seq2Seq(nn.Module):
-    def __init__(self):
+    """
+
+    """
+
+    def __init__(self, encoder, decoder):
+        """
+
+        :param encoder:
+        :param decoder:
+        """
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, source, target, english, device, teacher_force_ratio=0.5):
+        batch_size = source.shape[1]
+        target_len = target.shape[0]
+        target_vocab_size = len(english.vocab)
+
+        outputs = torch.zeros(target_len, batch_size, target_vocab_size).to(device)
+
+        hidden, cell = self.encoder(source)
+
+        x = target[0]
+
+        for t in range(1, target_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+
+            outputs[t] = output
+
+            best_guess = output.argmax(1)
+
+            x = target[t] if random.random() < teacher_force_ratio else best_guess
+
+        return outputs
