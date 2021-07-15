09b34731c9e2bc75361829eb63157bc6bff7f68d
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-15 15:50:25 -0500

    Implements main seq2seq module

---
+++ src/seq2seq/model/seq2seq_model.py
@@ -0,0 +1,14 @@
+""" Main seq2seq model
+
+Created: July 15, 2021
+Updated: July 15, 2021
+
+Description:
+    Implements the seq2seq model
+"""
+import torch
+import torch.nn as nn
+
+
+class Seq2Seq(nn.Module):
+    pass
