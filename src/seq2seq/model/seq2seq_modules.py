bc5a7ad71dafeb6650d0e84b03b917fd169a616c
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-15 15:56:58 -0500

    Finishes implementation of seq2seq encoder

--- src/seq2seq/model/seq2seq_modules.py
+++ src/seq2seq/model/seq2seq_modules.py
@@ -11,7 +11,21 @@ import torch.nn as nn
 
 
 class Encoder(nn.Module):
-    pass
+    def __init__(self, input_size, embedding_size, hidden_size, num_layers, dropout):
+        super(Encoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.dropout = nn.Dropout(dropout)
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=dropout)
+
+    def forward(self, x):
+        embedding = self.dropout(self.embedding(x))
+        outputs, (hidden, cell) = self.rnn(embedding)
+
+        return hidden, cell
+
 
 class Decoder(nn.Module):
     pass
